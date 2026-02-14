0594d5d04f869aa0d779dfe4bf17f04a4eac5511
Parvv04 <parzzabeth12@gmail.com>
2026-02-14 18:22:35 +0530

    solution for Top K Frequent Elements

---
+++ topKfrequentElements.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> frequencies = new HashMap<>();
+        PriorityQueue<Integer> minheap = new PriorityQueue<>((a,b) -> frequencies.get(a) - frequencies.get(b));
+        List<Integer> output = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            int freq = 0;
+            if(frequencies.containsKey(nums[i])){
+                freq = frequencies.get(nums[i]);
+                freq++;
+                frequencies.put(nums[i],freq);
+            }
+            else{
+                frequencies.put(nums[i],1);
+            }
+        }
+        for(Integer no : frequencies.keySet()){
+            minheap.add(no);
+            if(minheap.size()>k){
+                minheap.poll();
+            }
+        }
+        while(!minheap.isEmpty()){
+            output.add(minheap.poll());
+        }
+        int[] arr = new int[k];
+        for(int i = 0; i < k; i++){
+            arr[i] = output.get(i);
+        }
+        return arr;
+    }
+}
